ad2583d98bc8ebb7874b4940c3902adc6b1b1293
Freewind <nowindlee@gmail.com>
2020-07-08 18:37:54 +0800

    typescript-react-maximum-update-depth-exceeded-demo

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-TypeScript React Hello World Demo
+TypeScript React Maximum Update Depth Exceeded Demo
 =================================
+
+```
+Warning: Maximum update depth exceeded. This can happen when a component calls setState inside useEffect, 
+but useEffect either doesn't have a dependency array, or one of the dependencies changes on every render.
+```
+
+原因是在useEffect代码中修改了它自己依赖的值，导致useEffect会不断被执行。
 
 ```
 npm install
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,8 +1,19 @@
 //import * as React from 'react'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 export default function Hello() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count > 0) {
+      setCount(n => n + 1);
+    }
+  }, [count]);
+
   return <div>
     <h1>Hello React</h1>
+    <div>
+      <button onClick={() => setCount(1)}>{count}</button>
+    </div>
   </div>
 };
